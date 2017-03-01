b3c673a8db520ae5b877b2e5acacf439032942dd
calvingoc <calgoc@gmail.com>
2017-03-01 16:44:42 -0600

    adding the stuff

--- app/src/main/java/com/example/cgehredo/walkingthedog/MainActivity.java
+++ app/src/main/java/com/example/cgehredo/walkingthedog/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
